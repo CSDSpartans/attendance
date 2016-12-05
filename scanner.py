e23d2174009bea2b2b2f7f174078079cdfcfd9ad
Cole Vahey <rcvahey01@gmail.com>
2016-12-05 10:54:38 -0500

    save

---
+++ scanner.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+ 
+import skilstak.colors as c
+
+with open("students.js",r+) as students:
+    pri
---
+++ students.js
@@ -0,0 +1,14 @@
+var students = {
+  "9":
+    {"001932":"Cole Vahey",
+      "001933":"Collin Flannery",
+      "001934":"Logan Godfrey",
+      "001935":"Konrad Christian",
+      "001936":"William Muhlbach"},
+  "10":
+    {"001937":"John Doe 10th"},
+  "11":
+    {"001938":"John Doe 11th"},
+  "12":
+    {"001939":"John Doe 12th"}
+}
