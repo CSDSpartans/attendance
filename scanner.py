f2c4a8616539a6da3144c6e58f607aa34e352814
Cole Vahey <rcvahey01@gmail.com>
2016-12-06 09:12:29 -0500

    save

--- scanner.py
+++ scanner.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
  
 import skilstak.colors as c
+import json
 
-with open("students.js",r+) as students:
-    pri
+with open("students.json","r") as students:
+    numberkey = json.load(students)
+    for x,y in numberkey.items():
+        print(x + " is to --> " + str(y))
+
+studentfind = input("Please enter a student's name\n>> ").title()
+print(studentfind)
