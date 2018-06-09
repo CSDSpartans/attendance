ab3dc8111fcee978510cef79aef091a12f122310
colevahey <rcvahey01@gmail.com>
2018-06-08 20:49:15 -0400

    Progress with Jade! Very simple javascript to generate a static site with a table. Lots more I can do and soon!

--- serve.js
+++ serve.js
@@ -2,14 +2,29 @@ const express = require('express')
 const app = express()
 const jade = require("jade")
 
+let studentData = {
+  "present":["Cole Vahey","Gunnar Olsen"],
+  "late":["Jack Martin"],
+  "absent":["Soren Wilson"]
+}
+
 let jade_string = [
   "doctype html",
   "html",
   "    body",
-  "        table(id='studenttable', border='1')",
+  "        table(border='1' width='100%' style='text-align:center;font-size:2em;font-family:Lucida Grande')",
   "            tr",
-  "                td COLE",
-  "                td JAJA"
+  "                th(style='background-color:#9fff99') Present",
+  "                th(style='background-color:#fff899') Late",
+  "                th(style='background-color:#ff9999') Absent",
+  "            tr",
+  "                td Cole Vahey",
+  "                td Jack Martin",
+  "                td",
+  "            tr",
+  "                td Soren Wilson",
+  "                td",
+  "                td Jack Martin"
 ].join("\n")
 
 let fn = jade.compile(jade_string, {pretty:true})
