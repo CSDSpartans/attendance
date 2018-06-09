2b24ac3ae23115c7340fc7f787012e160e489b57
colevahey <rcvahey01@gmail.com>
2018-06-09 17:56:10 -0400

    Added the dictionary compatability in. This is looking very good. Hoping I can get my hands on my arduino soon so that I can check out the NodeJS to Arduino Serial plugin I found. I am very close to finishing this version of the program. Next up is checking compatability and trying to add time stamps to everything.

--- serve.js
+++ serve.js
@@ -3,34 +3,49 @@ const app = express()
 const jade = require("jade")
 
 let studentData = {
-  "present":["Cole Vahey","Gunnar Olsen"],
-  "late":["Jack Martin"],
-  "absent":["Soren Wilson"]
+  "present":["Adam Nordik","Jacob Smith"],
+  "late":["James Michael","Matthew Greene","Brian Rett"],
+  "absent":["Dennis Smith", "Sam Darnold"]
 }
 
-let jade_string = [
+let jade_lines = [
   "doctype html",
   "html",
   "    body",
-  "        table(border='1' width='100%' style='text-align:center;font-size:2em;font-family:Lucida Grande')",
+  "        table(border='1' width='100%' style='border:2px solid black;text-align:center;font-size:2em;font-family:Lucida Grande')",
   "            tr",
   "                th(style='background-color:#9fff99') Present",
   "                th(style='background-color:#fff899') Late",
-  "                th(style='background-color:#ff9999') Absent",
-  "            tr",
-  "                td Cole Vahey",
-  "                td Jack Martin",
-  "                td",
-  "            tr",
-  "                td Soren Wilson",
-  "                td",
-  "                td Jack Martin"
-].join("\n")
+  "                th(style='background-color:#ff9999') Absent"
+]
 
-let fn = jade.compile(jade_string, {pretty:true})
-console.log(fn())
+let generate = function(obj){
+  for (i = 0; i < Math.max(obj.present.length,obj.late.length,obj.absent.length); i++) {
+    p = ""
+    l = ""
+    a = ""
+    if (obj.present[i] != null) {
+      p = obj.present[i]
+    }
+    if (obj.late[i] != null) {
+      l = obj.late[i]
+    }
+    if (obj.absent[i] != null) {
+      a = obj.absent[i]
+    }
+    jade_lines.push("            tr")
+    jade_lines.push("                td "+p)
+    jade_lines.push("                td "+l)
+    jade_lines.push("                td "+a)
+  }
+}
 
-app.get('/', (req, res) => res.send(fn()))
+generate(studentData)
+
+let comp = jade.compile(jade_lines.join("\n"), {pretty:true})
+console.log(comp())
+
+app.get('/', (req, res) => res.send(comp()))
 
 let port = 3000
 app.listen(port, () => console.log('Served on port', port))
